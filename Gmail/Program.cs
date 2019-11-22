55fe32f90c2ebeb392900e064745d61c09c11b98
alexander <alexander@kleiotechnology.com>
2019-11-22 12:08:57 -0600

    Update Program.cs
    Implemented Batch reques for Gmail API issue #1

--- Gmail/Program.cs
+++ Gmail/Program.cs
@@ -1,11 +1,13 @@
 ﻿using Google.Apis.Auth.OAuth2;
 using Google.Apis.Gmail.v1;
 using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Requests;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -59,22 +61,25 @@ namespace Gmail
                 ApplicationName = "Gmail API .NET Quickstart",
             });
 
-            // List all messages.
-            var messages = await ListMessagesAsync(service, "me");
-            Console.WriteLine("Messages:");
-            if (messages != null && messages.Count > 0)
-            {
-                foreach (var messageItem in messages)
-                {
-                    var message = await GetMessagesAsync(service, messageItem.Id);
+            // Batch the list all messages.
+            var messages = await BatchMessagesAsync(service, "me");
 
-                    Console.WriteLine("{0} {1}", message.Id, message.Snippet);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No messages found.");
-            }
+            // List all messages.
+            //var messages = await ListMessagesAsync(service, "me");
+            //Console.WriteLine("Messages:");
+            //if (messages != null && messages.Count > 0)
+            //{
+            //    foreach (var messageItem in messages)
+            //    {
+            //        var message = await GetMessagesAsync(service, messageItem.Id);
+
+            //        Console.WriteLine("{0} {1}", message.Id, message.Snippet);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No messages found.");
+            //}
             Console.Read();
         }
 
@@ -84,7 +89,7 @@ namespace Gmail
         /// <param name="service">Gmail API service instance.</param>
         /// <param name="userId">User's email address. The special value "me"
         /// can be used to indicate the authenticated user.</param>
-        /// <param name="filter">String used to filter Messages returned.</param>
+        /// <param name="filter">string used to filter Messages returned.</param>
         public async Task<List<Message>> ListMessagesAsync(GmailService service, string userId, string filter = "")
         {
             var result = new List<Message>();
@@ -105,7 +110,7 @@ namespace Gmail
                 {
                     Console.WriteLine("An error occurred: " + e.Message);
                 }
-            } while (!String.IsNullOrEmpty(request.PageToken));
+            } while (!string.IsNullOrEmpty(request.PageToken));
 
             return result;
         }
@@ -124,6 +129,85 @@ namespace Gmail
             var message = await request.ExecuteAsync();
 
             return message;
+        }
+
+        /// <summary>
+        /// Barch the list all Messages of the user's mailbox matching the query.
+        /// </summary>
+        /// <param name="service">Gmail API service instance.</param>
+        /// <param name="userId">User's email address. The special value "me"
+        /// can be used to indicate the authenticated user.</param>
+        /// <param name="filter">string used to filter Messages returned.</param>
+        public async Task<Message[]> BatchMessagesAsync(GmailService service, string userId, string filter = "")
+        {
+            var messageList = new Dictionary<string, Message>();
+
+            // Define parameters of request.
+            var listRequest = service.Users.Messages.List(userId);
+            listRequest.Q = filter;
+            listRequest.MaxResults = 500; // Limit of messages to return (500 maximum)
+
+            Console.WriteLine("Messages:");
+
+            do
+            {
+                // Execute the request for return all messages 
+                try
+                {
+                    // Batch for each page returned by list (1000 maximum)
+                    var batchRequest = new BatchRequest(service);
+
+                    var response = await listRequest.ExecuteAsync();
+
+                    foreach (var messageItem in response.Messages)
+                    {
+                        messageList.Add(messageItem.Id, messageItem);
+
+                        // Create a get request for return the full message
+                        var messageRequest = service.Users.Messages.Get("me", messageItem.Id);
+                        messageRequest.Format = UsersResource.MessagesResource.GetRequest.FormatEnum.Full;
+
+                        // Set the request and callback
+                        batchRequest.Queue<Message>(
+                            messageRequest,
+                            (message, error, i, msg) =>
+                            {
+                                if (error == null)
+                                {
+                                    messageList[message.Id] = message;
+
+                                    Console.WriteLine("{0} {1}", message.Id, message.Snippet);
+                                }
+                                else
+                                {
+                                    RequestError requestError = error;
+
+                                    Console.WriteLine("ERROR: {0}", requestError.Message);
+                                }
+                                Console.WriteLine();
+                            }
+                        );
+                    }
+
+                    // Set the next page
+                    listRequest.PageToken = response.NextPageToken;
+
+
+                    await batchRequest.ExecuteAsync();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occurred: " + e.Message);
+                }
+            } while (!string.IsNullOrEmpty(listRequest.PageToken));
+
+            if (messageList.Count() == 0)
+            {
+                Console.WriteLine("No messages found.");
+            }
+
+
+            return messageList.Values.ToArray();
         }
     }
 }
